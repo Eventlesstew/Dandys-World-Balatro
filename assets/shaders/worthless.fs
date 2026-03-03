529ea99bb981104dade90d2884c73004cfbc2905
Eventlesstew <161836208+Eventlesstew@users.noreply.github.com>
2026-03-03 14:51:12 +1100

    Update worthless.fs

--- assets/shaders/worthless.fs
+++ assets/shaders/worthless.fs
@@ -4,7 +4,7 @@
 	#define MY_HIGHP_OR_MEDIUMP mediump
 #endif
 
-extern MY_HIGHP_OR_MEDIUMP vec2 dw_worthless;
+//extern MY_HIGHP_OR_MEDIUMP vec2 worthless;
 extern MY_HIGHP_OR_MEDIUMP number dissolve;
 extern MY_HIGHP_OR_MEDIUMP number time;
 extern MY_HIGHP_OR_MEDIUMP vec4 texture_details;
